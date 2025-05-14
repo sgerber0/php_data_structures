371be196c6ac10c4a1540560d3fd5cda54254c2f
Stefan Gerber <stefan.gerber197@myci.csuci.edu>
2025-05-14 16:39:08 -0700

    Created Stack with defaualt php array

---
+++ stack.php
@@ -0,0 +1,46 @@
+<?php class Stack {
+    protected $stack_array = array();
+    protected $stack_size = 0;
+
+    /* Adding a new item to the end of the stack */
+    /* Returns new size of stack */
+
+    public function push($item) {
+        $this->stack_array[$this->stack_size] = $item;
+            return ++$this->stack_size;
+    }
+
+    /* Removal of last element on the stack */
+    /* Returns that last element */
+
+    public function pop() {
+        if ($this->stack_size) {
+            $last_value = $this->stack_array[$this->stack_size - 1];
+            $this->stack_array[--$this->stack_size] = NULL;
+            return $last_value;
+        }
+        throw new Exception('Stack is empty.');
+    }
+
+    /* Preview of last element on the stack */
+    /* Returns the last element of the stack */
+
+    public function peek() {
+        if ($this->stack_size) {
+            return $this->stack_array[$this->stack_size - 1];
+        }
+        throw new Exception('Stack is empty.');
+    }
+
+    /* Returns number of element on the stack */
+
+    public function size(): int {
+        return $this->stack_size;
+    }
+
+    /* Return if stack is empty */
+
+    public function is_empty(): bool {
+        return ($this->stack_size === 0);
+    }
+} ?>
